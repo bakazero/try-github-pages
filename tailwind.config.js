d79abb44e66b9f203b3992fa49d4db84bb9bb546
bakazero <bobzzee@gmail.com>
2024-09-17 19:51:20 +0700

    coba lagi

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ function filterDefault(values) {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
