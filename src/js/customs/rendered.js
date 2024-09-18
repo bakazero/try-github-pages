0f9cc9aefc1baf69f1f9950fe4038f281c66bf69
bakazero <bobzzee@gmail.com>
2024-09-18 20:34:54 +0700

    ok

--- src/js/customs/rendered.js
+++ src/js/customs/rendered.js
@@ -1,5 +1,6 @@
 import { html, render } from "https://esm.run/uhtml@4.5.11";
 
 const container = document.createElement("div");
-const template = html` <div id="rendered" class="sr-only">rendered</div> `;
+document.body.append(container);
+const template = html` <div id="rendered" class="sr-only"></div> `;
 render(container, template);
